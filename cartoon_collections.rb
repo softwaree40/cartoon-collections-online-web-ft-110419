6e113e0d32906a4246823a4a1bceed43224dd6f3
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 17:53:50 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(dwarves) # code an argument here
+    dwarves.each_with_index do |dwarve, idx|
+      
+      puts (idx+1).to_s + dwarve
+      
+    end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+   new_created_array = planeteer_calls.map {|planeteer_call| planeteer_call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(short_words)# code an argument here
   # Your code here
+   short_words.each do |word|
+     #binding.pry
+    if word.length >= 4 
+      return false
+    else
+      return true
+     end
+   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  if cheese_types.include?(cheese_types.first)
+    #binding.pry
+     return cheese_types.first
+   else
+       !cheese_types.include?(cheese_types)
+    end
+  end
